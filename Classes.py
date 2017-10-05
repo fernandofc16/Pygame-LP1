4beaef928822e33276b935b13b66f70b08d697ec
Fernando Ferreira Cunha <fernandofc16@gmail.com>
2017-10-04 22:55:35 -0300

    Várias coisas

--- Classes.py
+++ Classes.py
@@ -9,13 +9,14 @@ class Monster():
         self.img = img
         self.rect = rect
 
-
 class Player():
 
     def __init__(self, position, img, rect):
         self.position = position
         self.img = img
         self.rect = rect
+        self.grau = 0
+        self.passo = 1
 
     def setPosition(self, position):
         self.position = position
@@ -27,36 +28,97 @@ class Player():
         self.rect = rect
 
     def rotateCenter(self, angle):
-        rot_image = pygame.transform.rotate(self.img, angle)
-        rot_rect = rot_image.get_rect(center=self.rect.center)
-        self.setImage(rot_image)
-        self.setRect(rot_rect)
+        loc = self.img.get_rect().center
+        rot_sprite = pygame.transform.rotate(self.img, angle)
+        rot_sprite.get_rect().center = loc
+        self.setImage(rot_sprite)
+        self.setRect(rot_sprite.get_rect())
+
+class Shot():
+
+    def __init__(self, position, eixo):
+        self.position = position
+        self.eixo = eixo
+        if eixo == 'x':
+            self.img = pygame.image.load('sprites_player/shotX.png')
+            self.rect = (0, 0, 9, 30)
+        else:
+            self.img = pygame.image.load('sprites_player/shotY.png')
+            self.rect = (0, 0, 30, 9)
+        self.vel = 20
+
+    def move(self):
+        if self.eixo == 'x':
+            self.position = (self.position[0]+self.vel, self.position[1])
+        else:
+            self.position = (self.position[0], self.position[1]+self.vel)
 
 pygame.init()
 screen = pygame.display.set_mode((1000, 700))
 screen.fill((255, 255, 255))
 
-img_player = pygame.image.load('sprite_player.jpg')
-player1 = Player((500-68/2, 350-90/2), img_player, (0, 0, 68, 90))
+img_player = pygame.image.load('sprites_player/sprite1_player_0.jpg')
+player1 = Player((500-70/2, 350-70/2), img_player, (0, 0, 70, 70))
 
 inGame = True
 
+shots = []
+
 while inGame:
     screen.fill((255, 255, 255))
+    
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_LEFT and event.key == pygame.K_UP:
-                player1.setPosition((player1.position[0]-10, player1.position[1]-10))
-            elif event.key == pygame.K_UP:
-                player1.setPosition((player1.position[0], player1.position[1]-10))
+            if event.key == pygame.K_UP:
+                if player1.grau == 0:
+                    player1.setPosition((player1.position[0], player1.position[1]-10))
+                elif player1.grau == 90:
+                    player1.setPosition((player1.position[0]+10, player1.position[1]))
+                elif player1.grau == 180:
+                    player1.setPosition((player1.position[0], player1.position[1]+10))
+                elif player1.grau == 270:
+                    player1.setPosition((player1.position[0]-10, player1.position[1]))
+                player1.passo += 1
             elif event.key == pygame.K_DOWN:
-                player1.setPosition((player1.position[0], player1.position[1]+10))
+                if player1.grau == 0:
+                    player1.setPosition((player1.position[0], player1.position[1]+10))
+                elif player1.grau == 90:
+                    player1.setPosition((player1.position[0]-10, player1.position[1]))
+                elif player1.grau == 180:
+                    player1.setPosition((player1.position[0], player1.position[1]-10))
+                elif player1.grau == 270:
+                    player1.setPosition((player1.position[0]+10, player1.position[1]))
+                player1.passo -= 1
             elif event.key == pygame.K_RIGHT:
                 #player1.setPosition((player1.position[0]+10, player1.position[1]))
-                player1.rotateCenter(math.radians(45))
+                player1.grau += 90
             elif event.key == pygame.K_LEFT:
                 #player1.setPosition((player1.position[0]-10, player1.position[1]))
-                player1.rotateCenter(math.radians(-45))
-            
+                player1.grau -= 90
+            elif event.key == pygame.K_SPACE:
+                if player1.grau == 0 or player1.grau == 180:
+                    shots.append(Shot(player1.position, 'y'))
+                else:
+                    shots.append(Shot(player1.position, 'x'))
+
+            for shot in shots:
+                shot.move()
+                if shot.position[0] > 1000 or shot.position[0] < 0:
+                    shots.remove(shot)
+                elif shot.position[1] > 700 or shot.position[1] < 0:
+                    shots.remove(shot)
+
+            if player1.grau >= 360:
+                player1.grau = 0
+            if player1.grau < 0:
+                player1.grau = 360 + player1.grau
+            if player1.passo > 3:
+                player1.passo = 1
+            if player1.passo < 1:
+                player1.passo = 3
+            player1.setImage(pygame.image.load('sprites_player/sprite' + str(player1.passo) + '_player_' + str(player1.grau) + '.jpg'))
+                   
     screen.blit(player1.img, player1.position, player1.rect)
     pygame.display.update()
+
+clock.tick(60)
