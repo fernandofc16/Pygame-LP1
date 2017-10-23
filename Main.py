e7bb7e62c455dfc494478a322f48f14ca8aa9401
fernandofc16 <fernandofc16@gmail.com>
2017-10-22 23:49:42 -0200

    Separated classes in modules

---
+++ Main.py
@@ -0,0 +1,54 @@
+import pygame
+import time
+from maps import Map
+
+pygame.init()
+
+game_map = Map()
+
+##poring_right_image = [pygame.image.load('sprites_monsters/poring/right/frame_' + str(i) + '_delay-0.1s.png') for i in range(10)]
+##poring_left_image = [pygame.image.load('sprites_monsters/poring/left/frame_' + str(i) + '_delay-0.1s.png') for i in range(10)]
+##
+##angeling_right_images = [pygame.image.load('sprites_allies/angeling/right/frame_' + str(i) + '_delay-0.15s.png') for i in range(27)]
+##angeling_left_images = [pygame.image.load('sprites_allies/angeling/left/frame_' + str(i) + '_delay-0.15s.png') for i in range(27)]
+
+game_map.spawnAllies(1, game_map.images.getAngelingImages(), 1)
+game_map.spawnMonsters(1, game_map.images.getPoringImages(), 1, False)
+
+#image = pygame.image.load('sprites_player/sprite' + str(player1.passo) + '_player_' + str(player1.grau) + '.png')
+
+while game_map.inGame:
+    pygame.time.Clock().tick(60)
+    game_map.screen.fill((255, 255, 255))
+    game_map.blitBackgroundMap()
+
+    game_map.checkEvents()
+    
+    game_map.player.animatePlayerSprite()
+
+    game_map.shotsInteractions()
+
+    game_map.bulletsInteractions()
+    
+    game_map.monstersInteractions()
+                    
+    game_map.alliesInteractions()
+                    
+    game_map.checkEndOfLevel()
+
+    game_map.showPlayerInfos()
+
+
+    if game_map.showGuiLevel:
+        if time.time() - game_map.start_time > 1:
+            game_map.showGuiLevel = False
+        game_map.showLevelGUI()
+
+    pygame.display.update()
+
+if game_map.win:
+    game_map.screen.blit(pygame.image.load('background_images/you_win.jpg'), (0, 0))
+else: 
+    game_map.screen.blit(pygame.image.load('background_images/game_over.png'), (0, 0))
+    
+pygame.display.update()
